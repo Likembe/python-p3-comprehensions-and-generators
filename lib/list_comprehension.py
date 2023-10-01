fbf2fed6b324b60bdfa30012f609eac8d4705882
Alpha <alpha.likembe@student.moringaschool.com>
2023-10-01 21:46:30 +0300

    complete

--- lib/list_comprehension.py
+++ lib/list_comprehension.py
@@ -1,7 +1,21 @@
 #!/usr/bin/env python3
 
 def return_evens(num_list):
-    pass
+    even_elements = [n for n in num_list if n % 2 == 0]
+    return even_elements
+
+#Example usage
+num_list = [0, 1, 3, 5, 7, 8, 9]
+result = return_evens(num_list)
+print(result)
 
 def make_exclamation(sentence_list):
-    pass
+    exclamation_sentence = [sentence + "!" for sentence in sentence_list]
+    return exclamation_sentence
+
+#Example
+
+input_sentence = ["Hello", "I'm doing great", "Python is fun"]
+result = make_exclamation(input_sentence)
+
+print(result)
